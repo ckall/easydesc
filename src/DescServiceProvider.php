358a8e634c6d1be9d4695af033d1269166aff7a4
admin <123>
2020-06-03 09:41:46 +0800

    update

---
+++ src/DescServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace CkallCloud;
+use Illuminate\Support\ServiceProvider;
+
+class DescServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        
+    }
+
+}
